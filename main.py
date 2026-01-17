e298221e223c95c0626914e07cd891cb5b6531ea
papa0601 <rapy0601@naver.com>
2026-01-18 00:42:00 +0900

    added search feature

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import requests
+import cloudscraper
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
 
-url = "https://vocaloid.fandom.com/wiki/"
+search_url = "https://vocaloid.fandom.com/api.php"
 
-song_name = 'lustrous'
+# song_name = input('Song Name: ')
+song_name = "antenna"
 
-song_name = song_name.replace(' ', '_')
+load_dotenv()
 
-response = requests.get(url + song_name)
+params = {
+    "action": "opensearch",
+    "search": song_name,
+    "limit": 1,
+    "format": "json"
+}
+
+scraper = cloudscraper.create_scraper()
+
+print(f"[검색 중...] {song_name}")
+search_result = scraper.get(search_url, params=params)
+if search_result.status_code != 200:
+    print("[오류] 검색에 실패했습니다!")
+    exit()
+print(f"[검색 성공] 곡 이름: {search_result.json()[1][0]}")
+url = search_result.json()[3][0]
+
+print(url)
+
+response = scraper.get(url)
 
 if response.status_code == 200:
     html = response.text
