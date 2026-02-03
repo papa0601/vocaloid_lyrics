aff17cfddff1999f456f3132e35f5d63f4216758
papa0601 <rapy0601@naver.com>
2026-02-04 00:31:28 +0900

    New Logic

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import json
 
 def make_local_cache(html, filename):
     with open(f"./{filename}.html", "w", encoding="utf-8") as f:
-        f.writelines(html)
+        f.write(html)
 
 def load_local_cache(filename) -> str:
     with open(f"./{filename}.html", "r", encoding="utf-8") as f:
-        return f.readlines() 
-
+        return ''.join(f.readlines()) 
+'''
 scraper = cloudscraper.create_scraper()
 search_url = "https://vocaloidlyrics.miraheze.org/w/api.php" # 검색할 위키의 엔드포인트
 song_name = "antenna" # 검색어
@@ -21,12 +21,12 @@ params = {
     "limit": 7,
     "format": "json"
 }
-SEARCH_TARGET_EXCEPTION = ("disambiguation", "album")
+SEARCH_TARGET_EXCEPTION = ("disambiguation", "album") # 검색 예외 단어 설정
 search_result = scraper.get(search_url, params=params)
-# Search Result 형식: ['검색어'], [검색된 문서의 풀네임], [정체 불명의 빈 리스트], [검색된 문서의 링크]]
+# search_result의 json 형식: ['검색어', [검색된 문서의 풀네임 리스트], [검색된 문서 수 길이의 빈 리스트], [검색된 문서의 링크 리스트]]
 
 if search_result.status_code != 200:
-    raise(Exception("[오류] 검색에 실패했습니다!"))
+    raise Exception("[오류] 검색에 실패했습니다!")
 
 search_result_json = search_result.json()
 song_document_name = None
@@ -41,17 +41,34 @@ for i in range(len(search_result_json[1])):
     song_document_link = search_result_json[3][i]
     break
 
-if not all((song_document_name, song_document_link)):
-    raise(Exception("[오류] 검색은 완료했으나, 노래를 찾을 수 없습니다!"))
+else:
+    raise Exception("[오류] 검색은 완료했으나, 노래를 찾을 수 없습니다!")
 
 print(f"[검색 성공] 문서 제목: {song_document_name} (링크: {song_document_link})")
 
 response = scraper.get(song_document_link)
 if response.status_code != 200:
-    raise(Exception("[오류] 검색한 문서를 불러오지 못했습니다!"))
+    raise Exception("[오류] 검색한 문서를 불러오지 못했습니다!")
 
 html = response.text
+
+make_local_cache(html, "antenna")
+'''
+html = load_local_cache("antenna")
 soup = BeautifulSoup(html, 'html.parser')
 
+lyrics_table = soup.select_one('[class="lyrics-table"]')
+lyrics_type_header = lyrics_table.select_one('[class="lyrics-table-header"]')
+lyrics_type_list = tuple(lyrics_type.text.strip() for lyrics_type in lyrics_type_header.select("th"))
 
+lyrics = []
+for i in range(len(lyrics_type_list)): lyrics.append([])
+lyric_lines = lyrics_table.select("tr")[1:] # 헤더 제외
 
+for lyric_line in lyric_lines:
+    part = lyric_line.select("td")
+    for idx in range(len(lyrics_type_list)):
+        target_text = part[idx].text if len(part) > idx else part[0].text
+        target_text = target_text.strip()
+        if target_text:
+            lyrics[idx].append(target_text)
