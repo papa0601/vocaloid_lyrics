29332baedbaba98cf020fbc345b497a517bcc0ea
papa0601 <rapy0601@naver.com>
2026-02-03 21:32:44 +0900

    Multiple Versions (Beta)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cloudscraper
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
+import json
 
 # Create output PATH if not exists
 if not os.path.exists('./output/'):
@@ -13,7 +14,7 @@ load_dotenv() # load .env file (not used)
 search_url = "https://vocaloid.fandom.com/api.php"
 
 # song_name = input('Song Name: ')
-song_name = "antenna 39"
+song_name = "M@GICAL"
 
 params = {
     "action": "opensearch",
@@ -21,6 +22,9 @@ params = {
     "limit": 1,
     "format": "json"
 }
+
+with open("./blacklist.json", "r", encoding="utf-8") as f:
+    blacklist = json.load(f)
 
 # Use cloudscraper to bypass cloudflare
 scraper = cloudscraper.create_scraper()
@@ -38,44 +42,60 @@ response = scraper.get(url)
 
 if response.status_code == 200:
     html = response.text
+    
+    # 실행하기 전에 이거 확인하셈 ㅇㅇ
+    with open("./test.html", "w", encoding="utf-8") as f:
+        f.writelines(html)
+    exit()
     soup = BeautifulSoup(html, 'html.parser')
     tables = soup.select(".mw-parser-output table")
+    lyrics_tables_origins = soup.select('[class="wds-tabs__tab-label"]')
 
-    lyrics_table = None
+    lyrics_table_names = []
+
+    
+    # 노래의 여러가지 버전이 존재할 경우 그것들의 이름을 추출합니다
+    for lyrics_tables_origin in lyrics_tables_origins:
+        print(lyrics_tables_origin)
+        if lyrics_tables_origin.text.strip() not in blacklist and lyrics_tables_origin.text.strip().find("Unofficial") == -1:
+            lyrics_table_names.append(lyrics_tables_origin.text.strip())
+    print(lyrics_table_names)
+
+    if not lyrics_table_names:
+        lyrics_table_names.append('Default')
+
+
+    lyrics_tables = []
+
+    for x in range(len(lyrics_table_names)):
+            if not os.path.exists(f"./output/{wiki_doc_name}{'/' + (lyrics_table_names[x]) if len(lyrics_table_names) > 1 else ''}"):
+                os.makedirs(f"./output/{wiki_doc_name}{'/' + (lyrics_table_names[x]) if len(lyrics_table_names) > 1 else ''}")
 
     for table in tables:
         headers = table.select("th")
         for header in headers:
-            if header.text.find("Japanese") != -1:
-                lyrics_table = table
+            if header.text.find("Japanese") != -1 or header.text.find("English") != -1:
+                lyrics_tables.append(table)
                 break
-        if lyrics_table:
-            break
+    print(len(lyrics_tables))
+    for i, lyrics_table in enumerate(lyrics_tables):
 
-    # print(lyrics)
+        rows = lyrics_table.find_all("tr")
+        lyrics_types = rows[0].select("th")
+        lyrics_type_string = []
+        for lyrics_type in lyrics_types:
+            lyrics_type_string.append(lyrics_type.text.strip())
+        rows = rows[1:]
 
-    rows = lyrics_table.find_all("tr")
-    # print(rows)
-
-    lyrics_types = rows[0].select("th")
-    lyrics_type_string = []
-    for lyrics_type in lyrics_types:
-        lyrics_type_string.append(lyrics_type.text.strip())
-
-    rows = rows[1:]
-
-    if not os.path.exists(f"./output/{wiki_doc_name}"):
-        os.makedirs(f"./output/{wiki_doc_name}")
-
-    for i in range(len(lyrics_type_string)):
-        print(lyrics_type_string[i] + '\n')
-        with open(f"./output/{wiki_doc_name}/{lyrics_type_string[i].strip()}.txt", "w", encoding="utf-8") as f:
-            for row in rows:
-                td = row.find_all("td")
-                if len(td) > i:
-                    f.write(td[i].text.strip() + '\n')
-                else:
-                    f.write(td[0].text.strip() + '\n')
+        for j in range(len(lyrics_type_string)):
+            print(lyrics_type_string[j] + '\n')
+            with open(f"./output/{wiki_doc_name}/{(lyrics_table_names[i] + '/') if len(lyrics_table_names) > 1 else ''}{lyrics_type_string[j].strip().replace(' ', '_')}.txt", "w", encoding="utf-8") as f:
+                for row in rows:
+                    td = row.find_all("td")
+                    if len(td) > j:
+                        f.write(td[j].text.strip() + '\n')
+                    else:
+                        f.write(td[0].text.strip() + '\n')
 
 
 else:
